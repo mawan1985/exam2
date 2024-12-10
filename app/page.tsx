c3399aa85f606639cbbde8b85bd2a43a5229254b
mawan1985 <muhammad.awan25@qmail.cuny.edu>
2024-12-10 00:04:00 +0000

    added date

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 export default function About() {
   return (
     <>
+      Date: 12/9/2024
       <h1 className="font-style: italic; m-4 text-orange-400"> Tis the Season of Swimming!</h1>
       <p className='font-serif mx-10 my-2 text-blue-300'>At Goldfish Swim School – Glendale, we are ready to teach your child how to swim, float, and tread water successfully. Our expert swim instructors lead all swim lessons with compassion and care. They want nothing more than to witness each child mature and grow in the water. With small class sizes capped off at 4 students, you can rest assured knowing your child is always receiving close supervision. Our smaller class sizes also allow us to optimize progress, boost confidence, and increase safety.</p>
     </>
