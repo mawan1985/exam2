83e157eddd30305b19a77c6ba328e5cc62a32b30
mawan1985 <muhammad.awan25@qmail.cuny.edu>
2024-12-10 01:35:03 +0000

    Add final

--- app/enroll/page.tsx
+++ app/enroll/page.tsx
@@ -9,25 +9,25 @@ export default function enroll() {
       <td>
       Lessons for the littlest fish</td>
       <td className='font-serif mx-10 text-orange-300'> 4-35 months</td>
-      <td><button>Select</button></td>
+      <td><button className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-orange-700 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Select</button></td>
       </tr>
       <tr> <td className='font-serif mx-10 text-orange-300'>Junior (beginner - intermediate)</td>
       <td>Dipping a toe into independent lessons</td>
-      <td className='font-serif mx-10 text-orange-300'>3-4 years</td><td><button>Select</button></td>
+      <td className='font-serif mx-10 text-orange-300'>3-4 years</td><td><button className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-orange-700 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Select</button></td>
       </tr> <tr> <td className='font-serif mx-10 text-orange-300'>Glider (beginner - intermediate)</td>
       <td>Learn swimming skills</td>
       <td className='font-serif mx-10 text-orange-300'>4-12 years</td>
-      <td><button>Select</button></td></tr>
+      <td><button className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-orange-700 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Select</button></td></tr>
       <tr>
       <td className='font-serif mx-10 text-orange-300'> Pro (intermediate - advanced)</td>
       <td> Comfortable with a variety of swim skills</td>
       <td className='font-serif mx-10 text-orange-300'>4.5-12 years</td>
-      <td><button>Select</button></td>
+      <td><button className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-orange-700 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Select</button></td>
       </tr>
       <tr><td className='font-serif mx-10 text-orange-300'>Swim Force (swim team)</td>
       <td>Future competitive swimmers</td>
       <td className='font-serif mx-10 text-orange-300'>5-12 years</td>
-      <td><button>Select</button></td></tr>
+      <td><button className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-orange-700 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Select</button></td></tr>
       </table>
     </>
   );
--- app/final/page.tsx
+++ app/final/page.tsx
@@ -1,7 +1,13 @@
 export default function page() {
     return (
       <>
-      <h1 className="text-3xl">Final Exam</h1>  
+      <h1 className="text-3xl ml-5">Final Exam</h1>  
+      <form>
+        <label htmlFor="product" className="ml-5">Product Description</label>
+        <input type="text" name="product" id="product" className="m-5 text-black bg-orange-400"/>
+        <br />
+        <button className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-orange-700 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 ml-10" type="submit">Save</button>
+      </form>
       </>
     );
   }
