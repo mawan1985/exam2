9a1db084613fc58a9091d1b1b1078ae24744bf5a
mawan1985 <muhammad.awan25@qmail.cuny.edu>
2024-12-15 23:10:00 +0000

    desc

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -6,6 +6,9 @@ export default function page() {
         <label htmlFor="product" className="ml-5">Student Name</label>
         <input type="text" name="product" id="product" className="m-5 text-black bg-orange-400"/>
         <br />
+        <label htmlFor="product" className="ml-5">Course Description</label>
+        <input type="text" name="product" id="product" className="m-5 text-black bg-orange-400"/>
+        <br />
         <button className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-orange-700 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 ml-10" type="submit">Save</button>
       </form>
       </>
