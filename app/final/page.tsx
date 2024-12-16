bcbe08ba519a41a464fd1f85a83825f4f0e902de
mawan1985 <muhammad.awan25@qmail.cuny.edu>
2024-12-16 02:56:25 +0000

    fin

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -1,13 +1,13 @@
 export default function page() {
     return (
       <>
-      <h1 className="text-3xl ml-5">Final Exam</h1>  
+      <h1 className="text-3xl ml-5 text-white">Final Exam</h1>  
       <form>
-        <label htmlFor="product" className="ml-5">Student Name</label>
-        <input type="text" name="product" id="product" className="m-5 text-black bg-orange-400"/>
+        <label htmlFor="product" className="ml-5 text-white">Student Name</label>
+        <input type="text" name="product" id="product" className="m-5 text-white bg-orange-400"/>
         <br />
-        <label htmlFor="product" className="ml-5">Course Description</label>
-        <input type="text" name="product" id="product" className="m-5 text-black bg-orange-400"/>
+        <label htmlFor="product" className="ml-5 text-white">Course Description</label>
+        <input type="text" name="product" id="product" className="m-5 text-white bg-orange-400"/>
         <br />
         <label htmlFor="instructors" className= "mx-5 text-white" >Choose your Instructor:</label>
         <select name="instructors" id="instructors">
@@ -18,7 +18,7 @@ export default function page() {
         </select>
         <br />
         <div className="ml-5 mt-5">
-        <h1 className="text-1xl mx-5">Choose your Schedule</h1> 
+        <h1 className="text-1xl mx-5 text-white">Choose your Schedule</h1> 
         <input type="radio" id="morning" name="schedule" value="MORNING" />
         <label htmlFor="morning">Morning</label><br />
         <input type="radio" id="afternoon" name="schedule" value="AFTERNOON" />
@@ -28,7 +28,7 @@ export default function page() {
         </div>
         <br />
         <div className="ml-5 mt-5"> 
-        <h1 className="text-1xl mx-5">Programs!!!</h1> 
+        <h1 className="text-1xl mx-5 text-white">Programs!!!</h1> 
         <input type="checkbox" id="program1" name="program1" value="Family" />
         <label htmlFor="program1"> Family Swim</label><br />
         <input type="checkbox" id="program2" name="program2" value="Free" />
